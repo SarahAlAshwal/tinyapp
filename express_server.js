d7241c82af891e2f8d0064425bf2963a6ffdbe88
SarahAlAshwal <sa.alashwal@gmail.com>
2020-08-31 17:48:02 +0000

    installing and setting up EJS

--- express_server.js
+++ express_server.js
@@ -2,13 +2,15 @@ const express = require ('express');
 const app = express();
 const PORT = 8080;
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = { 
-"b2xVn2": "http://www.lighthouselabs.ca",
-"9sm5xK": "http://www.google.com"
+'b2xVn2': 'http://www.lighthouselabs.ca',
+'9sm5xK': 'http://www.google.com'
 };
 
 app.get('/', (req, res) => {
-  res.send("Hello");
+  res.send('Hello');
 });
 
 app.get('/urls.json', (req, res) => {
@@ -16,7 +18,7 @@ app.get('/urls.json', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
 })
 
 app.listen(PORT, () => {
